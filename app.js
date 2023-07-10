785259f6afbf0c96f58981f7dfec1e4db346ac0a
username <Jzazzi56@gmail.comn>
2023-07-10 16:30:18 -0700

    oh jeez

---
+++ app.js
@@ -0,0 +1,14 @@
+const addBtn = document.querySelector(".increment");
+const minusBtn = document.querySelector(".decrement");
+
+addBtn.addEventListener('click') => {
+    for (let i = 0; i < addBtn; i++){
+        return addBtn++
+    }
+}
+
+minusBtn.addEventListener('click') => {
+    for (let i = 0; i > minusBtn; i--) {
+        return minusBtn--
+    }
+} 
